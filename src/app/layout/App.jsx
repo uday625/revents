a459d582b7f6c2a763f3b4a4bce60f598dd1a53c
Uday <uday625@outlook.com>
2018-07-10 05:20:46 +0000

    adds navbar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import NavBar from '../../features/event/EventDashboard/EventDashboard'
+import EventDashboard from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
-      <div >
-        <h1> Re-vents</h1>
-
+      <div>
+          <NavBar/>
+          <Container className="main">
+            <EventDashboard/>
+          </Container>
       </div>
     );
   }
